faedba03eb39fc4ffd278695ec4722c770e31754
Raritetnik <mkodarenko@gmail.com>
2023-10-03 10:51:57 -0400

    Initialization of the project

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <h1>Hello World!</h1>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
